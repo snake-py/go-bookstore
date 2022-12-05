6e4539b4ba019d4956fa1277376f92a942b6be1f
f.batti <fabio.batti93@gmail.com>
2022-12-05 20:55:47 +0100

    made test run

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
 	gorm.io/driver/mysql v1.4.4 // indirect
 	gorm.io/gorm v1.24.2 // indirect
 )
--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,6 +1,9 @@
 package config
 
 import (
+	"os"
+
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -10,11 +13,22 @@ var (
 )
 
 func Connect() {
-	d, err := gorm.Open(mysql.Open("user:password/DBName?charset=utf8&parseTime=True&loc=Local"), &gorm.Config{})
+	d, err := gorm.Open(mysql.Open(getDbConnectionString()), &gorm.Config{})
 	if err != nil {
 		panic("Failed to connect to database!")
 	}
 	db = d
+}
+
+func getDbConnectionString() string {
+	godotenv.Load(".env")
+	dbName := os.Getenv("DATABASE")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+
+	return dbUser + ":" + dbPassword + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName + "?charset=utf8mb4&parseTime=True&loc=Local"
 }
 
 func GetDB() *gorm.DB {
